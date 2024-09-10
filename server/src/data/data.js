62a16501adfcd650d9b4695e8ea3053540d288ed
Mike Tyson <115731701+larik-22@users.noreply.github.com>
2024-09-10 13:01:11 +0200

    Initial setup and packages

---
+++ server/src/data/data.js
@@ -0,0 +1,18 @@
+export const data = {
+    bids: [],
+    sticks: [
+        {
+            name: "wizard stick",
+            imageUrl: "https://utfs.io/f/199fd94f-51c3-4c00-ad30-3613e8444f9b-mxgjju.png"
+        },
+        {
+            name: "pistol stick",
+            imageUrl: "https://utfs.io/f/14d3c81f-4ef8-4460-a55f-1a47e2d2eda6-jm13wm.png",
+        },
+        {
+            name: "wand stick",
+            imageUrl: "https://utfs.io/f/fd0d6837-d565-4bd0-b1a0-6ff844e9613b-juxa5t.png"
+        },
+    ],
+    users: []
+}
